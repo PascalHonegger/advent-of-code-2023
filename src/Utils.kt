d98da6d478f2e5cbb5e38d6f3934f6e855ba70f0
Pascal Honegger <pascal.honegger@bluewin.ch>
2023-11-30 22:32:03 +0100

    Prepare Utils.kt based on 2022

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,11 +2,19 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String) = Path("src", "$name.txt")
+    .readLines()
+
+/**
+ * Reads whole text from the given input txt file.
+ */
+fun readTextInput(name: String) = Path("src", "$name.txt")
+    .readText().trim()
 
 /**
  * Converts string to md5 hash.
@@ -14,6 +22,17 @@ fun readInput(name: String) = Path("src/$name.txt").readLines()
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+/**
+ * Calculate product by multiplying all elements together
+ */
+fun Iterable<Int>.product() = reduce(Int::times)
+
+/**
+ * Calculate product by multiplying all elements together
+ */
+fun Iterable<Long>.product() = reduce(Long::times)
+
 
 /**
  * The cleaner shorthand for printing output.
